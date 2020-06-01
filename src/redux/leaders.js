d4c6617f523173829b60fbf68ad454e5ac385d71
satyam <satyam1008tonu@gmail.com>
2020-06-01 17:53:58 +0530

    Assignment 4

--- src/redux/leaders.js
+++ src/redux/leaders.js
@@ -1,25 +1,34 @@
-import * as ActionTypes from "./ActionTypes";
+import * as ActionTypes from './ActionTypes';
 
 export const Leaders = (
-  state = { isLoading: true, errMess: null, promotions: [] },
-  action
+	state = {
+		isLoading: true,
+		errMess: null,
+		leaders: [],
+	},
+	action
 ) => {
-  switch (action.type) {
-    case ActionTypes.ADD_LEADERS:
-      return {
-        ...state,
-        isLoading: false,
-        errMess: null,
-        leaders: action.payload,
-      };
+	switch (action.type) {
+		case ActionTypes.ADD_LEADERS:
+			return {
+				...state,
+				isLoading: false,
+				errMess: null,
+				leaders: action.payload,
+			};
 
-    case ActionTypes.LEADERS_LOADING:
-      return { ...state, isLoading: true, errMess: null, leaders: [] };
+		case ActionTypes.LEADERS_LOADING:
+			return { ...state, isLoading: true, errMess: null, leaders: [] };
 
-    case ActionTypes.LEADERS_FAILED:
-      return { ...state, isLoading: false, errMess: action.payload };
+		case ActionTypes.LEADERS_FAILED:
+			return {
+				...state,
+				isLoading: false,
+				errMess: action.payload,
+				leaders: [],
+			};
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 };
